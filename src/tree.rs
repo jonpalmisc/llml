13dd954a086ab19dfbfe1b09d17ef5513de0297e
Jon Palmisciano <jp@jonpalmisc.com>
2021-07-06 15:09:39 -0400

    Add node attribute support

--- src/tree.rs
+++ src/tree.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use crate::parser::{LtmlParser, Rule};
 use pest::iterators::Pair;
 
@@ -5,9 +7,20 @@ pub enum TreeNode {
     Root(Vec<TreeNode>),
     Element {
         name: String,
+        attributes: HashMap<String, String>,
         children: Vec<TreeNode>,
     },
     Literal(String),
+}
+
+fn format_attributes(map: &HashMap<String, String>) -> String {
+    let mut r = String::from(" ");
+
+    for (k, v) in map {
+        r.push_str(&format!("{}=\"{}\" ", k, v));
+    }
+
+    return r.trim_end().to_string();
 }
 
 impl TreeNode {
@@ -18,12 +31,17 @@ impl TreeNode {
                     c.print(0);
                 }
             }
-            Self::Element { name, children } => {
+            Self::Element {
+                name,
+                children,
+                attributes,
+            } => {
                 println!(
-                    "{}Element<{}, {}>",
+                    "{}Element<{}/{}{}>",
                     " ".repeat(level * 2),
                     name,
-                    children.len()
+                    children.len(),
+                    format_attributes(attributes),
                 );
 
                 for c in children {
@@ -49,17 +67,31 @@ impl TreeBuilder {
     fn node_from_pair(pair: Pair<Rule>) -> TreeNode {
         let mut name = String::from("NULL");
         let mut children: Vec<TreeNode> = vec![];
+        let mut attributes = HashMap::new();
 
         for p in pair.into_inner() {
             match p.as_rule() {
                 Rule::ElementName => name = String::from(p.as_str()),
+                Rule::AttributeList => {
+                    for i in p.into_inner() {
+                        let mut a = i.into_inner();
+                        let k = a.next().unwrap().as_str();
+                        let v = a.next().unwrap().as_str();
+
+                        attributes.insert(k.to_string(), v.to_string());
+                    }
+                }
                 Rule::Element => children.push(Self::node_from_pair(p)),
                 Rule::Literal => children.push(Self::node_from_literal(p)),
                 _ => (),
             }
         }
 
-        TreeNode::Element { name, children }
+        TreeNode::Element {
+            name,
+            attributes,
+            children,
+        }
     }
 
     pub fn from_parsed_file(pair: Pair<Rule>) -> TreeNode {
