048dac48c07965ec763bdf3d18f56e0c8d22b655
Jon Palmisciano <jp@jonpalmisc.com>
2021-07-06 13:57:33 -0400

    Combine `TreeNode` and `TreeNodeType`; add `TreeBuilder`

--- src/main.rs
+++ src/main.rs
@@ -10,84 +10,78 @@ use pest::{iterators::Pair, Parser};
 #[grammar = "grammar.pest"]
 pub struct LtmlParser;
 
-enum TreeNodeType {
-    Element(String),
+enum TreeNode {
+    Root(Vec<TreeNode>),
+    Element {
+        name: String,
+        children: Vec<TreeNode>,
+    },
     Literal(String),
 }
 
-struct TreeNode {
-    kind: TreeNodeType,
-    children: Vec<TreeNode>,
-}
-
 impl TreeNode {
-    fn from_literal(pair: Pair<Rule>) -> Self {
-        TreeNode {
-            kind: TreeNodeType::Literal(pair.as_str().to_string()),
-            children: vec![],
+    pub fn print(&self, level: usize) {
+        match &self {
+            Self::Root(children) => {
+                for c in children {
+                    c.print(0);
+                }
+            }
+            Self::Element { name, children } => {
+                println!(
+                    "{}Element<{}, {}>",
+                    " ".repeat(level * 2),
+                    name,
+                    children.len()
+                );
+
+                for c in children {
+                    c.print(level + 1);
+                }
+
+                println!("");
+            }
+            Self::Literal(s) => {
+                println!("{}Literal<{:?}>", " ".repeat(level * 2), s);
+            }
         }
     }
+}
 
-    fn from_pair(pair: Pair<Rule>) -> Self {
-        let mut node = TreeNode {
-            kind: TreeNodeType::Element("NULL".to_string()),
-            children: vec![],
-        };
+struct TreeBuilder;
+
+impl TreeBuilder {
+    fn node_from_literal(pair: Pair<Rule>) -> TreeNode {
+        TreeNode::Literal(String::from(pair.as_str()))
+    }
+
+    fn node_from_pair(pair: Pair<Rule>) -> TreeNode {
+        let mut name = String::from("NULL");
+        let mut children: Vec<TreeNode> = vec![];
 
         for p in pair.into_inner() {
             match p.as_rule() {
-                Rule::ElementName => node.kind = TreeNodeType::Element(p.as_str().to_string()),
-                Rule::Element => node.children.push(TreeNode::from_pair(p)),
-                Rule::Literal => node.children.push(TreeNode::from_literal(p)),
+                Rule::ElementName => name = String::from(p.as_str()),
+                Rule::Element => children.push(Self::node_from_pair(p)),
+                Rule::Literal => children.push(Self::node_from_literal(p)),
                 _ => (),
             }
         }
 
-        return node;
+        TreeNode::Element { name, children }
     }
 
-    pub fn from_file(pair: Pair<Rule>) -> Self {
+    pub fn from_parsed_file(pair: Pair<Rule>) -> TreeNode {
         if pair.as_rule() != Rule::File {
             panic!("Expected file rule");
         }
 
         let mut children: Vec<TreeNode> = vec![];
         for p in pair.into_inner() {
-            children.push(TreeNode::from_pair(p));
+            children.push(Self::node_from_pair(p));
         }
 
-        TreeNode {
-            kind: TreeNodeType::Element("ROOT".to_string()),
-            children: children,
-        }
-    }
-
-    fn internal_print(&self, level: usize) {
-        match &self.kind {
-            TreeNodeType::Element(t) => {
-                println!(
-                    "{}Element<{}, {}>",
-                    " ".repeat(level * 2),
-                    t,
-                    self.children.len()
-                );
-
-                for c in &self.children {
-                    c.internal_print(level + 1);
-                }
-
-                println!("");
-            }
-            TreeNodeType::Literal(s) => {
-                println!("{}Literal<{:?}>", " ".repeat(level * 2), s);
-            }
-        }
-    }
-
-    pub fn print(&self) {
-        for c in &self.children {
-            c.internal_print(0);
-        }
+        TreeNode::Root(children)
     }
 }
 
@@ -98,6 +92,6 @@ fn main() {
         .next()
         .unwrap();
 
-    let tree = TreeNode::from_file(file);
-    tree.print();
+    let tree = TreeBuilder::from_parsed_file(file);
+    tree.print(0);
 }
